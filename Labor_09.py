dafe4bb43be75e82a3728822e1c0c6bec71ad266
adamcsik <adamcsik@uniduna.hu>
2024-11-07 10:22:11 +0100

    tizedik változat

--- Labor_09.py
+++ Labor_09.py
@@ -3,12 +3,36 @@ from tkinter import *
 from tkinter import messagebox
 
 def beleptetes():
+    def ok_bevitel():
+        be_ablak.destroy()
+
+    def reg_kerese():
+        be_ablak.destroy()
+        regisztracio()
+
     be_ablak = Tk()
     be_ablak.title("Beléptetés")
 
+    f_nev_cimke = Label(be_ablak, text="A felhasználó neve (email):")
+    f_jelszo_cimke = Label(be_ablak, text="Jelszó:")
+    f_nev = StringVar()
+    f_nev.set("")
+    f_nev_be = Entry(be_ablak, textvariable=f_nev, width=20)
+    f_jelszo =StringVar()
+    f_jelszo.set("")
+    f_jelszo_be = Entry(be_ablak, textvariable=f_jelszo, width=20)
+
+    ok_gomb = Button(be_ablak, text="OK", command=ok_bevitel, width=10)
+    reg_gomb = Button(be_ablak, text="Regisztráció", command=reg_kerese)
+
+    f_nev_cimke.grid(row=0, column=0, sticky="E", padx=15, pady=5)
+    f_nev_be.grid(row=0, column=1)
+    f_jelszo_cimke.grid(row=1, column=0, sticky="E", padx=15, pady=5)
+    f_jelszo_be.grid(row=1, column=1)
+    reg_gomb.grid(row=3  , column=2, padx=15, pady=5)
+    ok_gomb.grid(row=3, column=0, pady=5)
+
     be_ablak.mainloop()
-
-
 
 def regisztracio():
 
@@ -20,12 +44,13 @@ def regisztracio():
 
     reg_ablak = Tk()
     reg_ablak.title("Regisztráció")
+    #reg_ablak.geometry("400x200")
 
     f_nev_cimke = Label(reg_ablak, text="A felhasználó neve (email):")
     f_jelszo_cimke = Label(reg_ablak, text="Jelszó:")
     f_jelszo2_cimke = Label(reg_ablak, text="Jelszó ismét:")
 
-    ok_gomb = Button(reg_ablak, text="OK", command=ok_bevitel)
+    ok_gomb = Button(reg_ablak, text="OK", command=ok_bevitel, width=10)
     jelszo_gen_gomb = Button(reg_ablak, text="Jelszó generálása", command=jelszo_gen)
 
     f_nev = StringVar()
@@ -38,14 +63,14 @@ def regisztracio():
     f_jelszo2.set("")
     f_jelszo2_be = Entry(reg_ablak, textvariable=f_jelszo2, width=20)
 
-    f_nev_cimke.pack()
-    f_nev_be.pack()
-    f_jelszo_cimke.pack()
-    f_jelszo_be.pack()
-    f_jelszo2_cimke.pack()
-    f_jelszo2_be.pack()
-    jelszo_gen_gomb.pack()
-    ok_gomb.pack()
+    f_nev_cimke.grid(row=0, column=0, sticky="E", padx=15, pady=5)
+    f_nev_be.grid(row=0, column=1)
+    f_jelszo_cimke.grid(row=1, column=0, sticky="E", padx=15, pady=5)
+    f_jelszo_be.grid(row=1, column=1)
+    f_jelszo2_cimke.grid(row=2, column=0, sticky="E", padx=15, pady=5)
+    f_jelszo2_be.grid(row=2, column=1)
+    jelszo_gen_gomb.grid(row=1  , column=3, padx=15, pady=5)
+    ok_gomb.grid(row=3, column=1, pady=5)
 
     reg_ablak.mainloop()
 
@@ -58,14 +83,14 @@ def sugo():
 # főprogram
 app = Tk()
 app.title("Dolgozói nyilvántartás")
-
+app.geometry("600x400")
 menulista = Menu(app)
 
-fajl = Menu(menulista)
-fajl.add_command(label="Beléptetés", command=beleptetes)
-fajl.add_command(label="Regisztráció", command=regisztracio)
-fajl.add_command(label="Kilépés", command=app.destroy)
-menulista.add_cascade(label="Fájl", menu=fajl)
+hozzaferes = Menu(menulista)
+hozzaferes.add_command(label="Beléptetés", command=beleptetes)
+hozzaferes.add_command(label="Regisztráció", command=regisztracio)
+hozzaferes.add_command(label="Kilépés", command=app.destroy)
+menulista.add_cascade(label="Fájl", menu=hozzaferes)
 
 egyeb = Menu(menulista)
 egyeb.add_command(label="Névjegy", command=nevjegy)
